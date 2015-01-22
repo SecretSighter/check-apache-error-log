c69c5f6f5a0c401f4e8b6696e3bcf6de8217a319
root <root@AHCL.(none)>
2015-01-22 11:50:34 -0700

    updating the config variables so that everything is only in one place at the beginning

--- checkErrorLog.php
+++ checkErrorLog.php
@@ -3,9 +3,6 @@ ini_set('include_path',ini_get('include_path').PATH_SEPARATOR.__DIR__);
 require_once('Zend'.DIRECTORY_SEPARATOR.'Loader'.DIRECTORY_SEPARATOR.'Autoloader.php');
 $loader = Zend_Loader_Autoloader::getInstance();
 
-$serverhash = hash_file('sha256', '/var/log/apache2/error.log');
-$localhash = hash_file('sha256', dirname(__FILE__).'/error.log');
-
 $rules = array(
 	'grep -v 172.31.17.131',
 	'grep -v 172.31.25.188',
@@ -13,39 +10,42 @@ $rules = array(
 	'grep -v Hostname'
 );
 
+$configEmail = 'douglas@americanhcl.com';
+$configError = '/var/log/apache2/error.log';
+$configLocalError = dirname(__FILE__).'/error.log';
+$configErrorDiff = dirname(__FILE__).'/error.diff';
+$hashAlgo = 'sha256';
+
+$serverhash = hash_file($hashAlgo, $configError);
+$localhash = hash_file($hashAlgo, $configLocalError);
+
 if($serverhash != $localhash){
 	$mail = new \Zend_Mail();
 	$mail->setSubject('Added Error to Error Log');
 	$mail->setFrom(php_uname('n'));
-	$mail->addTo('douglas@americanhcl.com');
-	copy('/var/log/apache2/error.log', dirname(__FILE__).'/error.log');
+	$mail->addTo($configEmail);
+	copy($configError, $configLocalError);
 
-	$firstDiffHash = hash_file('sha256', dirname(__FILE__) . '/error.diff');
+	$firstDiffHash = hash_file($hashAlgo, $configErrorDiff);
 
-	$executedString = 'cat ' . dirname(__FILE__) . '/error.log';
+	$executedString = 'cat ' . $configLocalError;
 	foreach($rules as $rule){
 		$executedString .= ' | ' . $rule . '';
 	}
-	$executedString .= ' > ' . dirname(__FILE__) . '/error.diff';
+	$executedString .= ' > ' . $configErrorDiff;
 
-//	echo $executedString . "\n";
-//	echo $firstDiffHash . "\n";
 	exec($executedString);
 
-//	echo hash_file('sha256', dirname(__FILE__).'/error.diff');
-	
-	if(hash_file('sha256', dirname(__FILE__).'/error.diff') != $firstDiffHash){
-	//	$mail->createAttachment(file_get_contents(dirname(__FILE__).'/error.diff'), 'text/plain', Zend_Mime::DISPOSITION_INLINE, Zend_Mime::ENCODING_BASE64, 'Error.log');
-
-	$lines=array();
-	$fp = fopen(dirname(__FILE__).'/error.diff', "r");
-	while(!feof($fp))
-	{
-	   $line = fgets($fp, 4096);
-	   array_push($lines, $line);
-	   if (count($lines)>5)
-	       array_shift($lines);
-	}
+	if(hash_file($hashAlgo, $configErrorDiff) != $firstDiffHash){
+		$lines=array();
+		$fp = fopen($configErrorDiff, "r");
+		while(!feof($fp))
+		{
+			$line = fgets($fp, 4096);
+			array_push($lines, $line);
+			if (count($lines)>5)
+				array_shift($lines);
+		}
 	fclose($fp);
 
 	$mail->setBodyText(implode("\n", $lines));
