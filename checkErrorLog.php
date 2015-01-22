bdde1620d55dc70f5bd862cb55c6d84d4de47094
root <root@AHCL.(none)>
2015-01-22 11:05:34 -0700

    updating to return last x lines

--- checkErrorLog.php
+++ checkErrorLog.php
@@ -6,17 +6,48 @@ $loader = Zend_Loader_Autoloader::getInstance();
 $serverhash = hash_file('sha256', '/var/log/apache2/error.log');
 $localhash = hash_file('sha256', dirname(__FILE__).'/error.log');
 
+$rules = array(
+	'grep -v 172.31.17.131',
+	'grep -v 172.31.25.188',
+	'grep -v Hostname'
+);
+
 if($serverhash != $localhash){
 	$mail = new \Zend_Mail();
 	$mail->setSubject('Added Error to Error Log');
 	$mail->setFrom(php_uname('n'));
 	$mail->addTo('douglas@americanhcl.com');
 	copy('/var/log/apache2/error.log', dirname(__FILE__).'/error.log');
-	exec('tail -100 ' . dirname(__FILE__).'/error.log > '.dirname(__FILE__).'/error.diff');
-	$mail->createAttachment(file_get_contents(dirname(__FILE__).'/error.diff'), 'text/plain', Zend_Mime::DISPOSITION_INLINE, Zend_Mime::ENCODING_BASE64, 'Error.log');
-	$mail->setBodyText('error.log');
-	$mail->send(new \Zend_Mail_Transport_Smtp('localhost'));	
+
+	$firstDiffHash = hash_file('sha256', dirname(__FILE__) . '/error.diff');
+
+	$executedString = 'cat ' . dirname(__FILE__) . '/error.log';
+	foreach($rules as $rule){
+		$executedString .= ' | ' . $rule . '';
+	}
+	$executedString .= ' > ' . dirname(__FILE__) . '/error.diff';
+
+//	echo $executedString . "\n";
+//	echo $firstDiffHash . "\n";
+	exec($executedString);
+
+//	echo hash_file('sha256', dirname(__FILE__).'/error.diff');
+	
+	if(hash_file('sha256', dirname(__FILE__).'/error.diff') != $firstDiffHash){
+	//	$mail->createAttachment(file_get_contents(dirname(__FILE__).'/error.diff'), 'text/plain', Zend_Mime::DISPOSITION_INLINE, Zend_Mime::ENCODING_BASE64, 'Error.log');
+
+	$lines=array();
+$fp = fopen(dirname(__FILE__).'/error.diff', "r");
+while(!feof($fp))
+{
+   $line = fgets($fp, 4096);
+   array_push($lines, $line);
+   if (count($lines)>5)
+       array_shift($lines);
 }
+fclose($fp);
 
-copy('/var/log/apache2/error.log', dirname(__FILE__).'/error.log');
-
+	$mail->setBodyText(implode("\n", $lines));
+		$mail->send(new \Zend_Mail_Transport_Smtp('localhost'));
+	}
+}
